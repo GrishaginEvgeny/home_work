e47f109119495dd11262919dbca9f126d50123fc
GrishaginEvgeny <thenoydota@gmail.com>
2023-01-10 16:51:30 +0300

    hw02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,51 +8,93 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
+func isScreeningByWithoutRepeat(prevStringedRune string, errActual error, stringedRune string) bool {
+	return prevStringedRune == "\\" && (errActual == nil || stringedRune == "\\")
+}
+
+func isError(errActual error, stringedRune string, errPrev error, prevPrevStringedRune string,
+	prevStringedRune string, index int,
+) bool {
+	return errActual == nil && stringedRune != "\\" &&
+		(errPrev == nil && prevPrevStringedRune != "\\" && prevStringedRune != "\\" || index == 0)
+}
+
+func getNextRune(index int, r string) string {
+	var nextRune string
+	if index != len(r)-1 {
+		nextRune = string(r[index+1])
+	} else {
+		nextRune = ""
+	}
+	return nextRune
+}
+
+func getPrevRune(index int, r string) string {
+	var prevRune string
+	if index != 0 {
+		prevRune = string(r[index-1])
+	} else {
+		prevRune = ""
+	}
+	return prevRune
+}
+
+func getPrevPrevRune(index int, r string) string {
+	var prevPrevRune string
+	if index > 1 {
+		prevPrevRune = string(r[index-2])
+	} else {
+		prevPrevRune = ""
+	}
+	return prevPrevRune
+}
+
+func getIncrementOfIndex(index int, size int) int {
+	var inc int
+	if index+2 > size-1 {
+		inc = 1
+	} else {
+		inc = 2
+	}
+	return inc
+}
+
 func Unpack(line string) (string, error) {
 	var result strings.Builder
-	r := []rune(line)
-	for i := 0; i < len(r); i++ {
-		prevStringedRune := ""
-		prevPrevStringedRune := ""
-		nextStringedRune := ""
-		stringedRune := string(r[i])
-		if i != 0 {
-			prevStringedRune = string(r[i-1])
-		}
-		if i != len(r)-1 {
-			nextStringedRune = string(r[i+1])
-		}
-		if i > 1 {
-			prevPrevStringedRune = string(r[i-2])
-		}
-
+	for i := 0; i < len(line); i++ {
+		prevStringedRune := getPrevRune(i, line)
+		prevPrevStringedRune := getPrevPrevRune(i, line)
+		nextStringedRune := getNextRune(i, line)
+		stringedRune := string(line[i])
 		intAct, errActual := strconv.Atoi(stringedRune)
 		_, errPrev := strconv.Atoi(prevStringedRune)
 		intNext, errNext := strconv.Atoi(nextStringedRune)
-
-		if prevStringedRune == "\\" && (errActual == nil || stringedRune == "\\") && errNext == nil {
-			result.WriteString(strings.Repeat(stringedRune, intNext))
-			if i+2 > len(r)-1 {
-				i++
-			} else {
-				i += 2
+		switch {
+		case prevStringedRune == "\\" && (errActual == nil || stringedRune == "\\") && errNext == nil:
+			{
+				result.WriteString(strings.Repeat(stringedRune, intNext))
+				i += getIncrementOfIndex(i, len(line))
+				break
 			}
-		} else if prevStringedRune == "\\" && (errActual == nil || stringedRune == "\\") {
-			result.WriteString(stringedRune)
-			i++
-		} else if errActual == nil && stringedRune != "\\" {
-			if i == 0 {
-				return "", ErrInvalidString
-			}
-			if errPrev == nil && prevPrevStringedRune != "\\" && prevStringedRune != "\\" {
-				return "", ErrInvalidString
-			}
-			if prevPrevStringedRune != "\\" {
-				result.WriteString(strings.Repeat(prevStringedRune, intAct))
-			}
-		} else {
-			if errNext != nil && prevStringedRune != "\\" && stringedRune != "\\" {
+		case isScreeningByWithoutRepeat(prevStringedRune, errActual, stringedRune):
+			{
 				result.WriteString(stringedRune)
+				i++
+				break
+			}
+		case isError(errActual, stringedRune, errPrev, prevPrevStringedRune, prevStringedRune, i):
+			{
+				return "", ErrInvalidString
+			}
+		case errActual == nil && stringedRune != "\\" && prevPrevStringedRune != "\\":
+			{
+				result.WriteString(strings.Repeat(prevStringedRune, intAct))
+				break
+			}
+		case errNext != nil && prevStringedRune != "\\" && stringedRune != "\\":
+			{
+				result.WriteString(stringedRune)
+				break
 			}
 		}
 	}
